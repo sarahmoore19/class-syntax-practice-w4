3a672ac30819564b6a24896a23bb086d31ac7a4d
sarahmoore <sarahnmoore19@gmail.com>
2022-10-19 11:43:33 -0400

    initial commit/solved

---
+++ classes/person.js
@@ -0,0 +1,37 @@
+class Person {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce() {
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+  }
+
+  static introducePeople(people) {
+    if (!Array.isArray(people)) {
+    throw new Error("introducePeople only takes an array as an argument.");
+    }
+
+    for (let ele of people) {
+        if (!(ele instanceof Person)) {
+    throw new Error("All items in array must be Person class instances.");
+
+        }
+    }
+
+    for (let ele of people) {
+      ele.introduce();
+    }
+  }
+}
+
+/****************************************************************************/
+/******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
+
+try {
+  module.exports = Person;
+} catch {
+  module.exports = null;
+}
